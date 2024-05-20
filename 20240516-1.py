8470d1c1d174e44a5dc647f7787edf6ffab2bc70
ChoiHongYeon <cgo0168@naver.com>
2024-05-20 15:45:08 +0900

    third commit

--- 20240516-1.py
+++ 20240516-1.py
@@ -12,14 +12,23 @@ def solution(a, b):
     s = (int(math.sqrt((b ** 2) / 2)) - (int(math.sqrt((a ** 2) / 2) + 1)) + 1) * 4 # xy좌표 같은 경우
 
     o = 0
-    for i in range(1, b):
-        for j in range(1, int(math.sqrt((b ** 2) / 2)) + 1):
-            if i > j and (i ** 2) + (j ** 2) >= a2 and (i ** 2) + (j ** 2) <= b2:
-                o += 1
+    for x in range(1, int(math.sqrt((b ** 2) / 2)) + 1):
+        min_y = 0
+        max_y = 0
+        for y in range(x + 1, b):
+            if x != y and (x ** 2) + (y ** 2) >= a2 and (x ** 2) + (y ** 2) <= b2:
+                min_y = y
+                break
+        for y in range(b - 1, x, -1):
+            if x != y and (x ** 2) + (y ** 2) >= a2 and (x ** 2) + (y ** 2) <= b2:
+                max_y = y
+                break
+        if min_y != 0 and max_y != 0:
+            o += (max_y - min_y + 1)
     o *= 8 # 그 외의 경우
     
     answer = z + s + o
     return answer
 
-result = solution(2,3)
+result = solution(2,4)
 print(result)
