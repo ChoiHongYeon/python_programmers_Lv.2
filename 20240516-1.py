9be096a7c4f7e3020b437bb34a934768a628f70b
ChoiHongYeon <cgo0168@naver.com>
2024-05-20 15:23:53 +0900

    second commit

--- 20240516-1.py
+++ 20240516-1.py
@@ -1,33 +1,25 @@
 # 두 원 사이의 정수 쌍
 
+import math
+
 def solution(a, b):
-   
-    a2 = pow(a, 2)
-    b2 = pow(b, 2)
-   
-    z = ((b - a) + 1) * 4 # xy좌표 하나라도 0
-   
-    min_s = 0
-    max_s = 0
-    for i in range(1, b + 1):
-        if pow(i, 2) * 2 >= a2 and pow(i, 2) * 2 <= b2:
-            min_s = i
-            break
-    for i in range(b, 0, -1):
-        if pow(i, 2) * 2 >= a2 and pow(i, 2) * 2 <= b2:
-            max_s = i
-            break
-    s = (max_s - min_s + 1) * 4 # xy좌표 같음
-   
+    
+    a2 = a ** 2
+    b2 = b ** 2
+
+    z = ((b - a) + 1) * 4 # xy좌표 하나라도 0인 경우
+
+    s = (int(math.sqrt((b ** 2) / 2)) - (int(math.sqrt((a ** 2) / 2) + 1)) + 1) * 4 # xy좌표 같은 경우
+
     o = 0
     for i in range(1, b):
-        for j in range(1, (int)((b + 1) / 2)):
-            if i != j and pow(i, 2) + pow(j, 2) >= a2 and pow(i, 2) + pow(j, 2) <= b2:
+        for j in range(1, int(math.sqrt((b ** 2) / 2)) + 1):
+            if i > j and (i ** 2) + (j ** 2) >= a2 and (i ** 2) + (j ** 2) <= b2:
                 o += 1
-    o *= 8
-   
+    o *= 8 # 그 외의 경우
+    
     answer = z + s + o
-   
     return answer
 
-print(solution(2,3))
+result = solution(2,3)
+print(result)
