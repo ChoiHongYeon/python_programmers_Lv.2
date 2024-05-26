b5eb7713373b21f84cc6578dbeeb6a148f9361d8
ChoiHongYeon <cgo0168@naver.com>
2024-05-26 23:15:41 +0900

    fourth commit

--- 20240526-1.py
+++ 20240526-1.py
@@ -1,24 +1,22 @@
 # 연속된 부분 수열의 합 (https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=python3)
 
 def solution(sequence, k):
-    
-    s = [sequence[len(sequence) - 1]]
-    s_max, s_min = len(sequence) - 1, len(sequence) - 1
-    
-    while sum(s) != k:
-        if sum(s) > k:
-            s.pop()
-            s_max -= 1
+
+    s = sequence[len(sequence) - 1]
+    s_max_index, s_min_index = len(sequence) - 1, len(sequence) - 1
+
+    while s != k:
+        if s > k:
+            s -= sequence[s_max_index]
+            s_max_index -= 1
         else:
-            s_min -= 1
-            s.insert(0, sequence[s_min])
+            s_min_index -= 1
+            s += sequence[s_min_index]
 
-    if max(s) == min(s):
-        return [sequence.index(s[0]), sequence.index(s[0]) + s_max - s_min]
+    if sequence[s_min_index] == sequence[s_max_index]:
+        return [sequence.index(sequence[s_max_index]), sequence.index(sequence[s_max_index]) + s_max_index - s_min_index]
     else:
-        return [s_min, s_max]
-
-
+        return [s_min_index, s_max_index]
 
 result1 = solution([1, 2, 3, 4, 5], 7)
 result2 = solution([1, 1, 1, 2, 3, 4, 5], 5)
