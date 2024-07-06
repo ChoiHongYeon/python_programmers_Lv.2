829f5b5842a0c4caf84da23fa006df1521f9e4b7
ChoiHongYeon <cgo0168@naver.com>
2024-07-06 21:58:18 +0900

    second commit

--- 20240705-1.py
+++ 20240705-1.py
@@ -2,28 +2,16 @@
 
 def solution(numbers):
 
-    up = []
-    for i in range(len(numbers) - 1):
-        if numbers[i] < numbers[i + 1]:
-            up.append(numbers[i + 1])
-
-    answer = []
-    for i in range(len(numbers) - 1):
-        if numbers[i] < numbers[i + 1]:
-            answer.append(numbers[i + 1])
-            del(up[0])
-        else:
-            if len(up) > 0:
-                for j in range(len(up)):
-                    if up[j] > numbers[i]:
-                        answer.append(up[j])
-            else:
-                answer.append(-1)
-        if i + 1 > len(answer):
-            answer.append(-1)
-
-    answer.append(-1)
-
+    answer = [-1] * len(numbers)
+    stack = []
+    
+    for i in range(len(numbers) - 1, -1, -1):
+        while len(stack) > 0 and stack[-1] <= numbers[i]:
+            stack.pop()
+        if len(stack) > 0:
+            answer[i] = stack[-1]
+        stack.append(numbers[i])
+    
     return answer
 
 result1 = solution([2, 3, 3, 5])
