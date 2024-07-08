1ef0d2d78ab147a35466d7d62a6df0f88d546fe4
ChoiHongYeon <cgo0168@naver.com>
2024-07-08 22:44:06 +0900

    second commit

--- 20240708-1.py
+++ 20240708-1.py
@@ -1,36 +1,28 @@
 # 숫자 변환하기 (https://school.programmers.co.kr/learn/courses/30/lessons/154538)
 
 def solution(x, y, n):
-    
-    answer = 0
-    OX = ["O", "O", "O"]
 
-    while y > x and "O" in OX:
+    answer = 0
+
+    while y > x:
+
         add_n = y
         mul_2 = y
         mul_3 = y
 
-        if OX[0] == "O":
-            if add_n >= x:
-                add_n = y - n
+        if y - n >= x:
+            add_n = y - n
+        if y % 2 == 0 and y // 2 >= x:
+            mul_2 = y // 2
+        if y % 3 == 0 and y // 3 >= x:
+            mul_3 = y // 3
 
-        if OX[1] == "O":
-            if y % 2 == 0 and y / 2 >= x:
-                mul_2 = y / 2
-            else:
-                OX[1] = "X"
-
-        if OX[2] == "O":
-            if y % 3 == 0 and y / 3 >= x:
-                mul_3 = y / 3
-            else:
-                OX[2] = "X"
-
-        y = min(add_n, mul_2, mul_3)
+        next_y = min(add_n, mul_2, mul_3)
+        if next_y == y:
+            answer = -1
+            break
+        y = next_y
         answer += 1
-
-    if x != y:
-        answer = -1
 
     return answer
 
